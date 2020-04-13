46f372d0020a4a683320ee75b63ec475f2141b4f
ayoubc <ayoubch807@gmail.com>
2020-04-13 09:38:09 +0100

    random-quote-machine

--- front-end-libraries-projects/random-quote-machine/src/App.js
+++ front-end-libraries-projects/random-quote-machine/src/App.js
@@ -1,36 +1,77 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import Quote from './components/quote/quote';
 
+const quoteURI = 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=';
+const apiKey = 'OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V';
+const colors = ['#16a085', '#27ae60', '#2c3e50', 
+                '#f39c12', '#e74c3c', '#9b59b6', 
+                '#FB6964', '#342224', "#472E32", 
+                "#BDBB99", "#77B1A9", "#73A857"];
 export class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      color: '#16a085',
+      text: '',
+      author: ''
+    }
+  }
 
+  componentWillMount() {
+    this.setNewQuote();
+  }
+
+  setNewQuote = () => {
+    this.getNewQuote().then(response => {
+      console.log(response);
+      this.setState({
+        color: this.getNewColor(),
+        text: response[0].quote,
+        author: response[0].author
+      })
+    }).catch(error => {
+      this.setState({
+        text: "Network Error",
+        author: ''
+      })
+    })
+  }
+
+  getNewColor = () => {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+  }
+
+  getNewQuote = () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 
+        'Content-Type': 'application/json',
+        'X-Mashape-Key': apiKey
+      }
+    }
+    return fetch(quoteURI, requestOptions)
+      .then(response => response.json())
+  }
   render() {
+    const appStyle = {
+      backgroundColor: this.state.color,
+      color: this.state.color
+    };
+
     return (
-      <div >
-
-        <div class="quote-box">
-          <div class="quote-text">
-            <i class="fa fa-quote-left"> </i><span id="text"></span>
-          </div>
-          <div class="quote-author">
-            - <span id="author"></span>
-          </div>
-          <div class="buttons">
-            <a class="button" id="tweet-quote" title="Tweet this quote!" target="_blank">
-              <i class="fa fa-twitter"></i>
-            </a>
-            <a class="button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank">
-              <i class="fa fa-tumblr"></i>
-            </a>
-            <button class="button" id="new-quote">New quote</button>
-          </div>
-        </div>
-        <div class="footer"> by <a href="http://codepen.io/hezag/">hezag</a></div>
-
+      <div className="App" style={appStyle}>
+        <Quote 
+          color={this.state.color}
+          text={this.state.text}
+          author={this.state.author}
+          onClick={this.setNewQuote}
+        />
       </div>
     );
   }
-
 }
 
 export default App;
--- front-end-libraries-projects/random-quote-machine/src/components/quote/quote.js
+++ front-end-libraries-projects/random-quote-machine/src/components/quote/quote.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-
 import './quote.css';
 
 export class Quote extends Component {
 
-  render() {
-    return (
-      <div >
+    constructor(props) {
+        super(props);
+    }
 
-      </div>
-    );
-  }
-
+    render() {
+        const btnStyle = {
+            backgroundColor: this.props.color
+        }
+        return (
+            <div className="wrapper">
+                <div id="quote-box">
+                    <p id="text">
+                        {this.props.text}
+                    </p>
+                    <div id="author">
+                        <span>- {this.props.author}</span>
+                    </div>
+                    <div className="buttons">
+                        <div id="social-media">
+                            <a id="tweet-quote">tweet</a>
+                        </div>
+                        <button id="new-quote" style={btnStyle} onClick={this.props.onClick}>
+                            New quote
+                        </button>
+                    </div>
+                </div>
+                <div className="footer">
+                    by <a href="https://codepen.io/ayub23" target="_blank">ayub23</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Quote;
